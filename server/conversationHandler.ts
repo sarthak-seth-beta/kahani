b336ab9e4d64619276a8126045fe71f3288ba1a1
Sarthak Seth <sarthakseth@Sarthaks-MacBook-Pro.local>
2025-12-08 01:22:29 +0530

    chore: retry at 5 mins instead of 4 hours

--- server/conversationHandler.ts
+++ server/conversationHandler.ts
@@ -438,7 +438,10 @@ async function handleReadinessResponse(
       throw error;
     }
   } else if (isMaybe) {
-    const retryAt = new Date(Date.now() + 4 * 60 * 60 * 1000);
+    // const retryAt = new Date(Date.now() + 4 * 60 * 60 * 1000);
+    const retryAt = new Date(Date.now() + 5 * 60 * 1000); // 5 mins
+
+
 
     await storage.updateFreeTrialDb(trial.id, {
       lastReadinessResponse: "maybe",
